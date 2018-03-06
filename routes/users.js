4f4724334cdcbff2294e3bc17e5d3810f155328e
Lucas Ferreira Pinto <lfp@oc3528502260.ibm.com>
2018-03-06 10:55:07 -0300

    alteracao na rota user e user/cool

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource'+ ' usando as coisas na aba de ususarios');
 });
 
+/* Ger users/cool. */
+router.get('/cool', function(req, res, next){
+  res.send('eu sou legal pra caramba');
+});
 module.exports = router;
